6954525bb11f9c0cb303742d48301cb88189acd9
水告木南 <Haonan_Fang@outlook.com>
2024-10-27 20:25:13 +0800

    feat:删除废弃的接口

--- cache/auth.go
+++ cache/auth.go
@@ -2,20 +2,11 @@ package cache
 
 import (
 	"context"
-	"fmt"
 	"github.com/go-redis/redis/v8"
 	"time"
 )
 
 type AuthCacheInterface interface {
-	// SetUserToken 设置用户token
-	// userID: 用户ID
-	// accessToken: 访问令牌
-	// refreshToken: 刷新令牌
-	// 如果设置成功, 返回nil
-	// 由于项目使用 Token 黑名单机制,因此该接口以已经废弃
-	SetUserToken(ctx context.Context, userID int64, accessToken, refreshToken string) error
-
 	// AddTokenToBlacklist 将token加入黑名单
 	// expiration: 过期时间
 	// token: token
@@ -35,16 +26,6 @@ type AuthCache struct {
 // NewAuthCache 实例化 AuthCache
 func NewAuthCache(client *redis.Client) AuthCacheInterface {
 	return &AuthCache{client}
-}
-
-func (a *AuthCache) SetUserToken(ctx context.Context, userId int64, accessToken string, refreshToken string) error {
-	//key := GenerateRedisKey(UserTokenKeyTemplate, userId)
-	//err := a.HSet(ctx, key, map[string]interface{}{
-	//	"accessToken":  accessToken,
-	//	"refreshToken": refreshToken,
-	//}).Err()
-	//return err
-	return fmt.Errorf(" SetUserToken(ctx context.Context, userId int64, accessToken string, refreshToken string) error 该接口已经废弃,请使用其他接口")
 }
 
 func (a *AuthCache) AddTokenToBlacklist(ctx context.Context, token string, expiration time.Duration) error {
